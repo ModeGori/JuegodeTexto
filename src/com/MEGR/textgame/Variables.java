c95937bc223c86ede741317cf45f331451c8e3a3
Unknown <moitix30@gmail.com>
2018-07-14 21:34:45 -0400

    Variables
    Scanners que varian

---
+++ src/com/MEGR/textgame/Variables.java
@@ -0,0 +1,19 @@
+package com.MEGR.textgame;
+import java.util.Scanner;
+public class Variables {
+	static Scanner input = new Scanner (System.in);
+	
+	public static String Nombre = ""; //Jugador
+	public static String Genero = ""; //No se puede ser homofobico
+	public static String Texto = ""; //Como moverse
+	public static String Respuesta = ""; //Respuestas en el juego
+	
+	public static void dormir ( ) {
+		try {
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
