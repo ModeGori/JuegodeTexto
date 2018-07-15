8880d0fe2bdf15a3aedb6e76be6a3ddc8d0a34bb
Unknown <moitix30@gmail.com>
2018-07-14 21:48:33 -0400

    int de estadisticas

--- src/com/MEGR/textgame/Jugador.java
+++ src/com/MEGR/textgame/Jugador.java
@@ -1,5 +1,10 @@
 package com.MEGR.textgame;
 
-public class Jugador {
+public class Jugador extends Variables {
+	public static int Vida = 100;
+	public static int Hambre = 100;
+	public static int Sed = 100;
+	public static int Energia = 100;
+	public static int EquilibrioMental = 100;
 
 }
