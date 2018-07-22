06e1c23d476880b8b7aad66dad06a39600736de6
Unknown <moitix30@gmail.com>
2018-07-22 15:20:04 -0400

    Clase lugares

---
+++ src/com/MEGR/textgame/Lugares.java
@@ -0,0 +1,13 @@
+package com.MEGR.textgame;
+
+public class Lugares {
+		void Bosque (){
+			int random = (int) (Math.random()*10);
+			 if (random < 20) {
+				 System.out.println("Estas en un bosque");
+			 } else if (random < 40) {
+				 System.out.println("");
+			 }
+		}
+
+	}
